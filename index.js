8cfe5aa5d3ec1152fc9740802192545a991bcb8c
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-03-30 19:18:22 +0300

    fix: return tlv value as binary by default

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function SmppParser(config, val, log) {
     this.patterns = {};
     this.opCodes = {};
     this.headerPattern = bitsyntax.parse('commandId:32/integer, commandStatus:32/integer, sequenceNumber:32/integer, body/binary');
-    this.tlvPattern = bitsyntax.parse('t:16/integer, l:16/integer, v:l/string, next/binary');
+    this.tlvPattern = bitsyntax.parse('t:16/integer, l:16/integer, v:l/binary, next/binary');
     this.init(config);
 }
 
